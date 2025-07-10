18bd115de912c8644276c3599ba3607a181a0d01
Igor Lunev <15331877+igolune@users.noreply.github.com>
2025-07-10 23:57:58 +0200

    Extend websocket client and add event types

--- internal/ws/events.go
+++ internal/ws/events.go
@@ -1,3 +1,18 @@
 package ws
 
-type UserEvent struct{}
+type EventType string
+
+// Defines values for EventType.
+const (
+	UserCreate   EventType = "io.github.guacamole_operator.user.success.create"
+	UserGet      EventType = "io.github.guacamole_operator.user.success.get"
+	UserUpdate   EventType = "io.github.guacamole_operator.user.success.update"
+	UserDelete   EventType = "io.github.guacamole_operator.user.success.delete"
+	Authenticate EventType = "io.github.guacamole_operator.authentication.success"
+)
+
+// UserData defines the content of user event.
+type UserData struct {
+	// Username of user.
+	Username string `json:"username"`
+}
--- internal/ws/ws.go
+++ internal/ws/ws.go
@@ -2,46 +2,58 @@ package ws
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 
-	cloudevents "github.com/cloudevents/sdk-go/v2"
 	"github.com/coder/websocket"
 )
 
 // Client implements a WebSocket client.
 type Client struct {
+	url  string
 	conn *websocket.Conn
 }
 
 // New returns a new Client.
-func New(conn *websocket.Conn) *Client {
+func New(url string) *Client {
 	return &Client{
-		conn: conn,
+		url: url,
 	}
 }
 
 // Read CloudEvents from the wire. Has to be executed in a goroutine.
-func (c *Client) Read(ctx context.Context, errCh chan<- error) {
+func (c *Client) Read(ctx context.Context, msgCh chan<- []byte, errCh chan<- error) {
 	for {
-		msgType, b, err := c.conn.Read(ctx)
+		_, b, err := c.conn.Read(ctx)
 		if err != nil {
 			errCh <- fmt.Errorf("error reading from websocket connection: %w", err)
+			err := c.Connect(ctx)
+			if err != nil {
+				errCh <- err
+			}
 			continue
 		}
-
-		// JSON expected.
-		if msgType != websocket.MessageText {
-			continue
-		}
-
-		// Read CloudEvent.
-		ce := cloudevents.NewEvent()
-
-		err = json.Unmarshal(b, &ce)
-		if err != nil {
-			errCh <- fmt.Errorf("error converting to CloudEvent: %w", err)
-			continue
-		}
+		msgCh <- b
 	}
+}
+
+func (c *Client) Close() error {
+	return c.conn.Close(websocket.StatusNormalClosure, "")
+}
+
+func (c *Client) Connect(ctx context.Context) error {
+	err := c.Close()
+	if err != nil {
+		return err
+	}
+
+	return c.Dial(ctx)
+}
+
+func (c *Client) Dial(ctx context.Context) error {
+	conn, _, err := websocket.Dial(ctx, c.url, nil)
+	if err != nil {
+		return err
+	}
+	c.conn = conn
+	return nil
 }
